09dbc62aa3e0298ca160223a284401eaf6af57de
Kareem Hegazy <kareemhegazy256@gmail.com>
2024-11-14 14:33:50 +0200

    adding background

--- obj/Debug/net8.0/RssAtomReader.AssemblyInfo.cs
+++ obj/Debug/net8.0/RssAtomReader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RssAtomReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3c65617445cc6d39c8881069dcb64a5627303e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RssAtomReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RssAtomReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
